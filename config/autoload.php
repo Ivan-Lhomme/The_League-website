5f51255cf9dc222c395b1b4f00f2305ef0c21a06
Ivan <i.lhommeageorges@gmail.com>
2025-12-05 14:41:40 +0100

    fix autoload

--- config/autoload.php
+++ config/autoload.php
@@ -2,7 +2,7 @@
 require "managers/AbstractManager.php";
 require "managers/GamesManager.php";
 require "managers/MediaManager.php";
-require "managers/PlyerManager.php";
+require "managers/PlayerManager.php";
 require "managers/PlayerPerformanceManager.php";
 require "managers/TeamManager.php";
 
