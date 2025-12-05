26602e92f98f98cf63ca327d52b9d1b19ec06d04
Ivan <i.lhommeageorges@gmail.com>
2025-12-05 14:25:55 +0100

    Update autoload

--- config/autoload.php
+++ config/autoload.php
@@ -1,4 +1,11 @@
 <?php
+require "managers/AbstractManager.php";
+require "managers/GamesManager.php";
+require "managers/MediaManager.php";
+require "managers/PlyerManager.php";
+require "managers/PlayerPerformanceManager.php";
+require "managers/TeamManager.php";
+
 require "controllers/AbstractController.php";
 require "controllers/PageController.php";
 
