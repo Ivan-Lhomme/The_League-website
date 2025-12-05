c6341ab0c5e02614054368a337333c905c7fe8d7
Ivan <i.lhommeageorges@gmail.com>
2025-12-05 15:47:29 +0100

    findOne return Image and findAll return array of Image

--- managers/MediaManager.php
+++ managers/MediaManager.php
@@ -1,18 +1,31 @@
 <?php
 class MediaManager extends AbstractManager{
-    public function findOne(int $id) : array {
+    public function findOne(int $id) : Image {
         $query = $this->db->prepare("SELECT * FROM media WHERE id = :id");
         $parameters = [
             "id" => $id
         ];
         $query->execute($parameters);
+        $image_data = $query->fetch(PDO::FETCH_ASSOC);
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $image = new Image($image_data["url"], $image_data["alt"]);
+        $image->setId($image_data["id"]);
+
+        return $image;
     }
     public function findAll() : array {
         $query = $this->db->prepare("SELECT * FROM media");
         $query->execute();
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $images_data = $query->fetchAll(PDO::FETCH_ASSOC);
+        $images = [];
+
+        foreach($images_data as $image_data) {
+            $image = new Image($image_data["url"], $image_data["alt"]);
+            $image->setId($image_data["id"]);
+            $images[] = $image;
+        }
+
+        return $images;
     }
 }
