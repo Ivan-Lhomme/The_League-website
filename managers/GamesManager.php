ed36070fa424273eb2e29fb78fd423a62e896b3f
Ivan <i.lhommeageorges@gmail.com>
2025-12-05 23:59:49 +0100

    findOne return Games and findAll return array of Games

--- managers/GamesManager.php
+++ managers/GamesManager.php
@@ -1,18 +1,33 @@
 <?php
 class GamesManager extends AbstractManager{
-    public function findOne(int $id) : array {
+    public function findOne(int $id) : Games {
         $query = $this->db->prepare("SELECT * FROM games WHERE id = :id");
         $parameters = [
             "id" => $id
         ];
         $query->execute($parameters);
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $game_data = $query->fetch(PDO::FETCH_ASSOC);
+        $teamManager = new TeamManager;
+
+        $game = new Games($game_data["name"], new DateTime($game_data["date"]), $teamManager->findOne($game_data["team_1"]), $teamManager->findOne($game_data["team_2"]), $game_data["winner"]);
+        $game->setId($game_data["id"]);
+        return $game;
     }
     public function findAll() : array {
         $query = $this->db->prepare("SELECT * FROM games");
         $query->execute();
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $games_data = $query->fetch(PDO::FETCH_ASSOC);
+        $games = [];
+        $teamManager = new TeamManager;
+
+        foreach($games_data as $game_data) {
+            $game = new Games($game_data["name"], new DateTime($game_data["date"]), $teamManager->findOne($game_data["team_1"]), $teamManager->findOne($game_data["team_2"]), $game_data["winner"]);
+            $game->setId($game_data["id"]);
+            $games[] = $game;
+        }
+
+        return $games;
     }
 }
