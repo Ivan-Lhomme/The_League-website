c8e3b4ad966a8a2e72ac910491ab533673aa875b
Pierre caillous <Pierre.cailloupgmail.com>
2025-12-05 15:49:08 +0100

    add team manager

--- managers/TeamManager.php
+++ managers/TeamManager.php
@@ -1,18 +1,34 @@
 <?php
 class TeamManager extends AbstractManager{
-    public function findOne(int $id) : array {
+    public function findOne(int $id) : Team {
         $query = $this->db->prepare("SELECT * FROM teams WHERE id = :id");
         $parameters = [
             "id" => $id
         ];
-        $query->execute($parameters);
+        $team_data = $query->fetch(PDO::FETCH_ASSOC);
+        $mediaManager = new MediaManager;
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $image = $mediaManager->findOne($team_data["id"]);
+        $team = new Player($team_data["name"], $team_data["description"], $image);
+        $team->setId($team_data["id"]);
+
+        return $team;
     }
     public function findAll() : array {
         $query = $this->db->prepare("SELECT * FROM teams");
         $query->execute();
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $teams_data = $query->fetchAll(PDO::FETCH_ASSOC);
+        $teams = [];
+        $mediaManager = new MediaManager;
+
+        foreach ($teams_data as $team_data) {
+            $image = $mediaManager->findOne($team_data["id"]);
+            $p = new Player($team_data["name"], $team_data["description"], $image);
+            $p->setId($team_data["id"]);
+            $teams[] = $p;
+        }
+
+        return $teams;
     }
 }
