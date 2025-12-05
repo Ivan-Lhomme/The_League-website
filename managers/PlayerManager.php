0598ee199486b5f992f723a183e07a473fceaeb0
Ivan <i.lhommeageorges@gmail.com>
2025-12-05 15:35:29 +0100

    findOne return Player and findAll return array of Player

--- managers/PlayerManager.php
+++ managers/PlayerManager.php
@@ -1,18 +1,36 @@
 <?php
 class PlayerManager extends AbstractManager{
-    public function findOne(int $id) : array {
+    public function findOne(int $id) : Player {
         $query = $this->db->prepare("SELECT * FROM players WHERE id = :id");
         $parameters = [
             "id" => $id
         ];
         $query->execute($parameters);
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $player_data = $query->fetch(PDO::FETCH_ASSOC);
+        $mediaManager = new MediaManager;
+
+        $image = $mediaManager->findOne($player_data["id"]);
+        $player = new Player($player_data["nickname"], $player_data["bio"], $image, $player_data["team"]);
+        $player->setId($player_data["id"]);
+
+        return $player;
     }
     public function findAll() : array {
         $query = $this->db->prepare("SELECT * FROM players");
         $query->execute();
 
-        return $query->fetch(PDO::FETCH_ASSOC);
+        $players_data = $query->fetch(PDO::FETCH_ASSOC);
+        $players = [];
+        $mediaManager = new MediaManager;
+        
+        foreach ($players_data as $player_data) {
+            $image = $mediaManager->findOne($player_data["id"]);
+            $p = new Player($player_data["nickname"], $player_data["bio"], $image, $player_data["team"]);
+            $p->setId($player_data["id"]);
+            $players[] = $p;
+        }
+
+        return $players;
     }
 }
