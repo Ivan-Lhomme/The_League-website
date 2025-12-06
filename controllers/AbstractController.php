4480728aa7f44332daaeca1e1a5d0341ff437862
Ivan <i.lhommeageorges@gmail.com>
2025-12-06 23:09:00 +0100

    Remove weird extract

--- controllers/AbstractController.php
+++ controllers/AbstractController.php
@@ -1,7 +1,6 @@
 <?php
     abstract class AbstractController {
         public function render(array $data, string $template) : void{
-            extract($data);  /*add this to create variables from array key for the home.phtml, it works, so I won't touch*/
             require "templates/layout.phtml";
         }
     }
