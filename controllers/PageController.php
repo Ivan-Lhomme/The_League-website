096cf13349cac958ff3b239793abb8575b9083b2
Pierre caillous <Pierre.cailloupgmail.com>
2025-12-07 10:44:34 +0100

    finish match page

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -17,16 +17,36 @@ class PageController extends AbstractController {
 
         $this->render(['featuredPlayers' => $featuredPlayers, 'lastGame' => $lastGame, 'featuredTeam' => $featuredTeam], 'home');
     }
-    public function match(int $id) {
+    public function match(int $id)
+    {
         $gamesManager = new GamesManager;
         $playerManager = new PlayerManager;
         $playerPerformanceManager = new PlayerPerformanceManager;
 
-        $player = $playerManager->findOne($id);
-        $player->setStats($playerPerformanceManager->findAll($player->getId()));
+        $game = $gamesManager->findOne($id);
+        $allPlayers = $playerManager->findAll();
 
-        $this->render([$gamesManager->findOne($id)], 'match');
+        $team1Players = [];
+        $team2Players = [];
+
+        foreach ($allPlayers as $p) {
+            if ($p->getTeam()['id'] == $game->getTeam_1()->getId()) {
+                $team1Players[] = $p;
+            }
+            if ($p->getTeam()['id'] == $game->getTeam_2()->getId()) {
+                $team2Players[] = $p;
+            }
+        }
+        foreach ($team1Players as $p) {
+            $p->setStats($playerPerformanceManager->findAll($p->getId()));
+        }
+        foreach ($team2Players as $p) {
+            $p->setStats($playerPerformanceManager->findAll($p->getId()));
+        }
+        $this->render(["game" => $game, "team1Players" => $team1Players, "team2Players" => $team2Players], "match");
     }
+
+
     public function matchs() {
         $gamesManager = new GamesManager;
         $this->render($gamesManager->findAll(), 'matchs');
